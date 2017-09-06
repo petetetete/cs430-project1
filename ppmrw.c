10390fc0d17ae99f205e803ee03b6a3a97cce535
Peter <huettlpeter@gmail.com>
2017-09-06 14:31:24 -0700

    Add getNextString helper function to skip whitespace and fetch the next 'string' from a file

--- ppmrw.c
+++ ppmrw.c
@@ -1,6 +1,44 @@
 // Include ppmrw header file
 #include "ppmrw.h"
 
+
+int getNextString(char *output, FILE *fileHandle) {
+
+  char symbol;
+
+  // Skip leading white space
+  while (1) {
+    symbol = fgetc(fileHandle);
+
+    // If we hit an EOF before a non-whitespace character
+    if (symbol == EOF) {
+      return NO_STRING_FOUND;
+    }
+
+    // Save first non-whitespace character we hit
+    else if (!isspace(symbol)) {
+      snprintf(output, STRING_MAX_BUFFER, "%s%c", output, symbol);
+      break;
+    }
+  }
+
+  // Copy all characters to the output until we hit another whitespace or EOF
+  while ((symbol = fgetc(fileHandle)) != EOF && !isspace(symbol)) {
+    sprintf(output, "%s%c", output, symbol);
+  }
+
+  // If we instantly hit EOF after skipping whitespace, there was no real string
+  if (symbol == EOF) {
+    return NO_STRING_FOUND;
+  }
+
+  // String successfully found
+  else {
+    return 0;
+  }
+
+}
+
 int main(int argc, char *argv[]) {
 
   // Check for the appropriate number of parameters
@@ -19,17 +57,30 @@ int main(int argc, char *argv[]) {
   char *inputFName = argv[2];
   char *outputFName = argv[3];
 
+  // Initialize variables to be used in program
+  FILE *inputFH;
+  char *magicNumber;
+
   // Validate conversion number
   if (convertToFormat != 3 && convertToFormat != 6) {
     fprintf(stderr, "Error: Invalid PPM format number, use 3 or 6\n");
     return 1;
   }
 
-  FILE *inputFH;
+  // Validate input file name
   if (!(inputFH = fopen(inputFName, "r"))) {
     fprintf(stderr, "Error: No input file of name '%s' found\n", inputFName);
     return 1;
   }
+
+  // Test finding magic number
+  magicNumber = malloc(sizeof(char) * STRING_MAX_BUFFER);
+  getNextString(magicNumber, inputFH);
+  printf(magicNumber);
+
+
+  // Clean up program
+  fclose(inputFH);
 
   return 0;
 }
--- ppmrw.h
+++ ppmrw.h
@@ -1,9 +1,16 @@
 #ifndef PPMRW_H
 #define PPMRW_H
 
+// Error code constants
+#define NO_STRING_FOUND -1
+
+// Numeric constants
+#define STRING_MAX_BUFFER 32
+
 // Include standard libraries
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h> // isspace
 
 // Define types to be used in ppmrw.c 
 typedef struct Pixel Pixel;
@@ -11,5 +18,15 @@ typedef struct Pixel Pixel;
 struct Pixel {
   unsigned char r, g, b, a;
 };
+
+/**
+ * Gets next series of ASCII characters from a file handle
+ * while ignoring leading whitespace
+ * 
+ * @param  output     character pointer to memory to save output
+ * @param  fileHandle file to be parsed for next string
+ * @return            success status of function, NO_STRING_FOUND on error
+ */
+int getNextString(char *output, FILE *fileHandle);
 
 #endif  // PPMRW_H
